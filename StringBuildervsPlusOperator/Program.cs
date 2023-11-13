0500a47c77137adf9006bf1f60a96a496c9ea711
serkutYILDIRIM <serkutyldrm@gmail.com>
2023-11-13 23:48:36 +0300

    StringBuilder and '+' operator console results.

--- StringBuildervsPlusOperator/Program.cs
+++ StringBuildervsPlusOperator/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using StringBuildervsPlusOperator;
+
+
+
+var operation = new StringConcatenationOperations();
+
+Console.WriteLine("String Builder Result: ");
+Console.WriteLine(operation.StringConcatenationUsingStringBuilder());
+Console.WriteLine("Plus Operator Result: ");
+Console.WriteLine(operation.StringConcatenationUsingPlusOperator());
+
+Console.Read();
+
---
+++ StringBuildervsPlusOperator/StringConcatenationOperations.cs
@@ -0,0 +1,35 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Text;
+
+namespace StringBuildervsPlusOperator
+{
+    [MemoryDiagnoser(false)]
+    public class StringConcatenationOperations
+    {
+        [Benchmark]
+        public string StringConcatenationUsingStringBuilder()
+        {
+            var stringBuilder = new StringBuilder();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                stringBuilder.Append("Serkut Yildirim" + i);
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        [Benchmark]
+        public string StringConcatenationUsingPlusOperator()
+        {
+            var finalString = string.Empty;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                finalString += "Serkut Yildirim" + i;
+            }
+
+            return finalString;
+        }
+    }
+}
